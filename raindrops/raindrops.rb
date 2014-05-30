0ae91017fefa7b60a6b234a943ab929daeb10c80
Lauren Scott <wisconsinpoet@gmail.com>
2014-05-30 11:47:41 -0500

    first working implementation of raindrops

---
+++ raindrops/raindrops.rb
@@ -0,0 +1,18 @@
+# - If the number contains 3 as a prime factor, output 'Pling'.
+# - If the number contains 5 as a prime factor, output 'Plang'.
+# - If the number contains 7 as a prime factor, output 'Plong'.
+# - If the number does not contain 3, 5, or 7 as a prime factor,
+#   just pass the number's digits straight through.
+
+
+class Raindrops
+
+	def self.convert(number)
+		raindrop_speak = ''
+		raindrop_speak << 'Pling' if number % 3 == 0
+		raindrop_speak << 'Plang' if number % 5 == 0
+		raindrop_speak << 'Plong' if number % 7 == 0
+		raindrop_speak << number.to_s if raindrop_speak.empty?
+		raindrop_speak
+	end
+end
